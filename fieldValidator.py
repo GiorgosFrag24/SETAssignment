39804c666cdb05fba4e35cdc319e93503405d060
GiorgosFrag <frag.giorgos24@gmail.com>
2023-08-15 21:22:12 +0300

    Created field validator class

---
+++ fieldValidator.py
@@ -0,0 +1,26 @@
+import re
+import logging
+
+
+class FieldValidator:
+
+    def __init__(self, expectedFieldValues, testData):
+        self.expectedFieldValues = expectedFieldValues
+        self.testData = testData
+
+    def validateJsonFieldPresence(self, fieldName):
+        logging.info(f'Expecting field {fieldName} to be present in response')
+        assert fieldName in self.testData
+
+    def validateJsonFieldType(self, fieldName):
+
+        fieldValue = self.testData[fieldName]
+        expectedValue = self.expectedFieldValues[fieldName]
+        if isinstance(expectedValue, str) and re.match(expectedValue, fieldValue):
+            assert re.match(expectedValue, fieldValue)
+        elif isinstance(expectedValue, list) and all(isinstance(pattern, str) for pattern in expectedValue):
+            assert isinstance(fieldValue, list)
+            for url, pattern in zip(fieldValue, expectedValue):
+                assert re.match(pattern, url)
+        else:
+            assert isinstance(fieldValue, expectedValue)
--- testGetPeople.py
+++ testGetPeople.py
@@ -1,18 +1,41 @@
-import pytest
-import requests
 import logging
+from fieldValidator import FieldValidator
+import json
+
+expectedFields = {
+    "name": str,
+    "height": r'^\d+$',
+    "mass": r'^\d+$',
+    "hair_color": str,
+    "skin_color": str,
+    "eye_color": str,
+    "birth_year": str,
+    "gender": str,
+    "homeworld": r'^https?://[\S]+$',
+    "films": [r'^https?://[\S]+$'],
+    "species": [],
+    "vehicles": [r'^https?://[\S]+$'],
+    "starships": [r'^https?://[\S]+$'],
+    "created": r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z$',
+    "edited": r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z$',
+    "url": r'^https?://[\S]+$'
+}
 
 
 def testGetPeopleHappyPath(client):
-    logging.info('Fetching the person with id:1')
-    response = client.get('/people/1/')
     logging.info('Calling GET /people/1/')
+    response = client.get('/people/1/')
+    logging.info('Expecting response code to equal 200')
     assert response.status_code == 200
-    logging.info('Successful Response')
+    fieldValidator = FieldValidator(expectedFields, json.loads(response.text))
+    for fieldName, expectedValue in expectedFields.items():
+        logging.info(f'Expecting response body to contain field {fieldName} of type {expectedValue}')
+        fieldValidator.validateJsonFieldPresence(fieldName)
+        fieldValidator.validateJsonFieldType(fieldName)
 
 
 def testGetPeopleNoId(client):
-    logging.info('Fetching the person with id:1')
+    logging.info('Calling GET /people/1/')
     response = client.get('/people/1/')
     logging.info('Calling GET /people/1/')
     assert response.status_code == 200
@@ -25,4 +48,3 @@ def testGetPeopleInvalidId(client):
     logging.info('Calling GET /people/234/')
     assert response.status_code == 404
     logging.info(response.text)
-
